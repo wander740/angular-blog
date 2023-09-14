dbdb41447444a70f81599f890c3b9a9b1a55fc1a
Wanderson Ramos <wandersonramos740@gmail.com>
2023-09-13 22:35:43 -0300

    changing css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'content/:id',
-    component: ContentComponent
-  }
+  { path: '', component: HomeComponent},
+  { path: 'content/:id', component: ContentComponent}
 ];
 
 @NgModule({
--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,7 +2,7 @@ export const dataFake = [
   {
     "id":"1",
     "title":"NOVO HOME DE FERRO ANUNCIADO",
-    "description":"Marvel estudios aniciam um novo homen de ferro.",
+    "description":"Marvel estudios aniciam um novo homen de ferro. Marvel estudios aniciam um novo homen de ferro. Marvel estudios aniciam um novo homen de ferro. Marvel estudios aniciam um novo homen de ferro. Marvel estudios aniciam um novo homen de ferro.Marvel estudios aniciam um novo homen de ferro.Marvel estudios aniciam um novo homen de ferro.Marvel estudios aniciam um novo homen de ferro.",
     "photoCover":"https://wallpapers.com/images/hd/iron-man-pictures-k83lqhe6vnh95b1m.jpg"
   },
   {
@@ -10,5 +10,11 @@ export const dataFake = [
     "title":"Nova série anunciadae",
     "description":"ola",
     "photoCover":"https://miro.medium.com/v2/resize:fit:1358/0*qdHImq1G588SB9Ii.jpg"
+  },
+  {
+    "id":"3",
+    "title":"Marvel Studios está sigilosamente trabalhando em novas animações",
+    "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus voluptatibus unde sed nobis! Voluptatem, numquam nesciunt ipsum, vitae vero officia quis sed delectus officiis illum libero rerum autem! Molestiae, repellat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, voluptates doloribus. Exercitationem sint nostrum culpa, esse deleniti asperiores distinctio repudiandae earum laudantium alias aliquam unde ex excepturi voluptates quos ut?",
+    "photoCover":"https://jamesons.com.br/wp-content/uploads/2021/11/marvel-studios-mini-animacao-capa.jpg"
   },
 ]
